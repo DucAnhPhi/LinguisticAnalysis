e5f6447f5cd7fda98be10e896ff2b43ed67df79d
Duc Anh Phi <ducanh.phi@hybridheroes.de>
2017-08-20 21:56:04 +0200

    feat(flesch_kincaid): get most and least readable tweets with their readability score

--- flesch_kincaid.py
+++ flesch_kincaid.py
@@ -10,6 +10,7 @@ Compute Flesch Kincaid readability tests
 
 import copy
 import utils
+from nltk.corpus import cmudict
 
 def normalize(text):
     normalizedText = copy.copy(text)
@@ -33,30 +34,68 @@ def normalize(text):
     # return normalized text tokenized
     normalizedText = utils.remove_special_characters(normalizedText)
 
-    return normalizedText
+    normalizedText = utils.remove_empty_sentences(normalizedText)
 
-def get_flesch_readability_ease(tokenizedText):
+    if not len(text):
+        return []
+    else:
+        return normalizedText
+
+def get_flesch_readability_ease(tokenizedText, pronouncingDict):
     words = sum(tokenizedText, [])
     numberOfWords = len(words)
     numberOfSentences = len(tokenizedText)
     numberOfSyllables = 0
     for word in words:
-        numberOfSyllables += utils.get_word_syllables(word)
+        numberOfSyllables += utils.get_word_syllables_offline(word, pronouncingDict)
     return 206.835 - (1.015 * numberOfWords / numberOfSentences) - (84.6 * numberOfSyllables / numberOfWords)
 
-def get_flesch_grade_level(tokenizedText):
+def get_flesch_grade_level(tokenizedText, pronouncingDict):
     words = sum(tokenizedText, [])
     numberOfWords = len(words)
     numberOfSentences = len(tokenizedText)
     numberOfSyllables = 0
     for word in words:
-        numberOfSyllables += utils.get_word_syllables(word)
+        numberOfSyllables += utils.get_word_syllables_offline(word, pronouncingDict)
     return (0.39 * numberOfWords / numberOfSentences) + (11.8 * numberOfSyllables / numberOfWords) - 15.59
 
+def get_text_with_max_or_min_readability_score(corpus, function, pronouncingDict, getMax):
+    maxMinScore = 'init'
+    maxMinScoreText = ''
+    for text in corpus:
+        score = function(text, pronouncingDict)
+        if maxMinScore == 'init':
+            maxMinScore = score
+            maxMinScoreText = text
+        else:
+            if getMax and score > maxMinScore:
+                maxMinScore = score
+                maxMinScoreText = text
+            elif not getMax and score < maxMinScore:
+                maxMinScore = score
+                maxMinScoreText = text
+    return (maxMinScore, maxMinScoreText)
+
 if __name__ == '__main__':
-    #tweets = utils.get_twitter_corpus()
-    #normalizedTweets = [ normalize(tweet) for tweet in tweets ]
-    normalized = normalize('The Australian platypus is seemingly a hybrid of a mammal and reptilian creature.')
-    print(normalized)
-    print(get_flesch_readability_ease(normalized))
-    print(get_flesch_grade_level(normalized))
+    pronouncingDict = cmudict.dict()
+    tweets = utils.get_twitter_corpus()
+    normalizedTweets = [ normalize(tweet) for tweet in tweets if len(normalize(tweet)) ]
+
+    # get text with lowest flesch readability ease score
+    minEase = get_text_with_max_or_min_readability_score(normalizedTweets, get_flesch_readability_ease, pronouncingDict, False)
+    print(minEase)
+    # get text with lowest flesch grade level score
+    minGrade = get_text_with_max_or_min_readability_score(normalizedTweets, get_flesch_grade_level, pronouncingDict, False)
+    print(minGrade)
+
+    # get text with highest flesch readability ease score
+    maxEase = get_text_with_max_or_min_readability_score(normalizedTweets, get_flesch_readability_ease, pronouncingDict, True)
+    print(maxEase)
+    # get text with highest flesch grade level score
+    maxGrade = get_text_with_max_or_min_readability_score(normalizedTweets, get_flesch_grade_level, pronouncingDict, True)
+    print(maxGrade)
+
+#    normalized = normalize('The Australian platypus is seemingly a hybrid of a mammal and reptilian creature.')
+#    print(normalized)
+#    print(get_flesch_readability_ease(normalized))
+#    print(get_flesch_grade_level(normalized))
