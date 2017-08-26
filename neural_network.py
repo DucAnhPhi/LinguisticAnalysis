5279f90ab76bc9a2c7b3054e5c0820445b2f53fb
Duc Anh Phi <ducanh.phi@hybridheroes.de>
2017-08-26 21:39:14 +0200

    feat(neural_network): add ability to specify number of hidden layers

--- neural_network.py
+++ neural_network.py
@@ -1,16 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Aug 25 20:09:08 2017
-
-@author: duc
-"""
-
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
 Created on Thu Aug 24 20:30:37 2017
-
+Neural network for supervised machine learning
 @author: duc
 """
 
@@ -36,18 +28,19 @@ class NeuralNetwork:
     
     bias = 1
     
-    def __init__(self, trainingIterations, learnRate, inputNeurons, outputNeurons):
-        
+    def __init__(self, trainingIterations, learnRate, inputNeurons, hiddenLayers, outputNeurons, data):
+
         self.trainingIterations = trainingIterations
         self.learnRate = learnRate
-        
-        # intialize amount of input and output neurons
+
+        # intialize amount of input, output and hidden neurons
         self.inputNeurons = inputNeurons
         self.outputNeurons = outputNeurons
+        self.hiddenLayers = hiddenLayers
 
         # initialize input data
-        data = dataset.getInput2()
         self.input = data[0]
+        print("input:\n",self.input)
         # input nodes
         self.inputColumns = data[0].shape[1]
         # amount of input data
@@ -55,76 +48,68 @@ class NeuralNetwork:
         
         # initialize output data
         self.output = data[1]
-        
+        #self.output = np.array([[0.5], [0.35]])
+        print("output:\n",self.output)
+
         # seed random generator
-        np.random.seed(7)
-        
+        #np.random.seed(7)
+
         # initialize weights randomly from -0.5 to 0.5
-        self.theta1 = np.random.rand(self.inputColumns + 1, inputNeurons)-0.5
-        self.theta2 = np.random.rand(inputNeurons + 1, outputNeurons)-0.5
-        print("initial theta1=\n",self.theta1)
-        print("initial theta2=\n",self.theta2)
-        
+        self.thetas = []
+        for i in range(0, hiddenLayers):
+            self.thetas.append(np.random.rand(self.inputColumns + 1, inputNeurons)-0.5)
+            print("initial theta", i+1, ":\n", self.thetas[i])
+        self.thetas.append(np.random.rand(inputNeurons + 1, outputNeurons)-0.5)
+        print("initial theta", hiddenLayers+1,":\n",self.thetas[hiddenLayers])
+
     def train(self):
-        
+
         for i in range(0, self.trainingIterations):
             # do forwardpass
-            
-            # input layer with bias
-            vb1 = concat_bias(self.bias, self.input)
-            # activation of hidden layer
-            a2 = sigmoid(dot(vb1, self.theta1))
-            # activation of hidden layer with bias
-            ab2 = concat_bias(self.bias, a2)
-            # output layer
-            a3 = sigmoid(dot(ab2, self.theta2))
+            fp = self.forward_pass(self.input)
 
             # update weights/thetas for next iteration
-            updatedThetas = self.get_updated_thetas(vb1, ab2, a2, a3)
-            self.theta1 = updatedThetas[0]
-            self.theta2 = updatedThetas[1]
-        
-#        print("Training complete")
-#        print("Theta1:\n", self.theta1)
-#        print("Theta2:\n", self.theta2)
-#        print("Theta3:\n", self.theta3)
-        # input layer with bias
-        vb1 = concat_bias(self.bias, self.input)
-        # activation of hidden layer
-        a2 = sigmoid(dot(vb1, self.theta1))
-        # activation of hidden layer with bias
-        ab2 = concat_bias(self.bias, a2)
-        # output layer
-        a3 = sigmoid(dot(ab2, self.theta2))
+            self.update_thetas(fp)
 
-        print("Prediction =\n", a3)
+        print("Prediction =\n", self.forward_pass(self.input)[-1])
 
-    def get_updated_thetas(self, vb1, ab2, a2, a3):
+    def forward_pass(self, inputData):
+        # store activations
+        a = []
+        # input layer with bias at index 0
+        a.append(inputData)
+        for i in range(0, len(self.thetas)):
+            # activation of a layer
+            a.append(sigmoid(dot(concat_bias(self.bias, a[i]), self.thetas[i])))
+
+        # return activations
+        return a
+
+    def update_thetas(self, a):
         ratio = self.learnRate / self.m
-        
+
         # do backpropagation
-        
-        # thetas transposed without bias
-        theta2 = self.theta2.T[:, 1:len(self.theta2.T[0])]
-        
-        #reuseable deltas
-        delta = ((a3 - self.output) * dsigmoid(a3))
-        
-        # compute updated weights
-        newTheta2 = self.theta2 - (ratio * dot(ab2.T, delta))
-        newTheta1 = self.theta1 - (ratio * dot(vb1.T, dot(delta, theta2) * dsigmoid(a2)))
-        
-        return (newTheta1, newTheta2)
-        
+
+        # all thetas transposed without bias except first theta
+        tempThetas = []
+        for i in range(1, len(self.thetas)):
+            tempThetas.append(self.thetas[i].T[:, 1:len(self.thetas[i].T[0])])
+
+        # compute all deltas
+        # initialize delta array
+        d = [0] * len(self.thetas)
+        # initialize first delta at last position
+        d[-1] = (a[-1] - self.output) * dsigmoid(a[-1])
+        #print("delta\n",d[-1])
+        # compute as many deltas as there are thetas
+        for j in range(len(self.thetas)-2, -1, -1):
+            d[j] = dot(d[j+1], tempThetas[j]) * dsigmoid(a[j+1])
+
+        # compute change and update weights
+        for k in range(len(self.thetas)-1, -1, -1):
+            self.thetas[k] = self.thetas[k] - (ratio * dot(concat_bias(self.bias, a[k]).T, d[k]))
 
 if __name__ == '__main__':
     np.set_printoptions(threshold = 1000, precision=4, suppress=True)
-    nn = NeuralNetwork(5000, 1, 2, 2)
+    nn = NeuralNetwork(3000, 1, 2, 1, 1, dataset.getInput2())
     nn.train()
-#    matrix = np.array([[0,0,0],[1,1,1],[-1,-1,-1]])
-#    print(dsigmoid(matrix))
-#    print(matrix)
-#    bias = np.array([[1],[1],[1]])
-#    print(concat_bias(matrix, 1))
-#    print(matrix)
-    
