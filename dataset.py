654b063ddbaa3c045db80b6b0219cd4d35373049
Duc Anh Phi <ducanh.phi@hybridheroes.de>
2017-08-26 21:55:47 +0200

    refactor(dataset): remove unused functions

--- dataset.py
+++ dataset.py
@@ -3,22 +3,16 @@
 """
 Created on Fri Aug 25 18:23:39 2017
 
+Prepare datasets for neural_network.py
+
 @author: duc
 """
 
 import numpy as np
 
-def getInput():
-    input = np.array([[0,0,0],[1,0,0],[0,1,0],[1,1,0],[0,0,1],[1,0,1],[0,1,1],[1,1,1]])
-    output = np.array([[0,0,0],[0,0,1],[0,0,1],[0,0,0],[0,0,0],[0,1,0],[0,1,0],[0,0,0]])
-    return(input, output)
 
-def getInput2():
+def getXOR():
     input = np.array([[0,0],[1,0],[0,1],[1,1]])
     output = np.array([[0],[1],[1],[0]])
     return(input, output)
 
-def getInput3():
-    input = np.array([[0.35, 0.9], [0.1, -0.7]])
-    output = np.array([[0.5],[0.35]])
-    return (input,output)
