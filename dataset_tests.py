be7d45f80de6b7d4921b4fe08a2ee0fc26b9166a
Duc Anh Phi <ducanh.phi@hybridheroes.de>
2017-08-28 23:52:04 +0200

    style(dataset_tests): fix spacing

--- dataset_tests.py
+++ dataset_tests.py
@@ -24,19 +24,19 @@ class DatasetTests(unittest.TestCase):
         norm = np.array([[1, 1, 1, 1], [0, 0.5, 0.4, 0.222], [0, 0.25, 0.02, 0.888], [1, 0.625, 0.6, 0.333]])
         decimal = 3
         np.testing.assert_array_almost_equal(ds.normalize(m), norm, decimal)
-    
+
     def test_combined_keywords(self):
         t1 = [[["fake", "media"]], [["fake", "news"]]]
         t2 = [[["women", "hillary"]], [["media", "trump"]]]
         keywords = set(["fake", "media", "news", "women", "hillary", "trump"])
         self.assertEqual(ds.get_combined_keywords(t1, t2), keywords)
-        
+
     def test_keywords_count(self):
         t =  [["make", "america", "great", "again"],["america", "was", "great"]]
         dict = {"make": 0, "america": 0, "great": 0, "again": 0, "was": 0}
         counted = {"make": 1, "america": 2, "great": 2, "again": 1, "was": 1}
         self.assertEqual(ds.get_keywords_count(t, dict), counted)
-        
+
     def test_extract_features(self):
         t =  ["Make america great again!", "America was great! Hillary Clinton"]
         norm = [[["make", "america", "great", "again"]],[["america", "was", "great"], ["hillary", "clinton"]]]
@@ -45,20 +45,20 @@ class DatasetTests(unittest.TestCase):
                 [4, 1, lvl(norm[0], pronDict), 1, 1, 1, 1, 0, 0, 0],
                 [2.5, 1, lvl(norm[1], pronDict), 0, 1, 1, 0, 1, 1, 1]
         ]
-        print(features)
         self.assertEqual(ds.extract_features(t, norm, count, pronDict), features)
-    
+
     def test_positive_negative_amount(self):
         m = [[0, 1, 0.5, 1, 0.02], [1, 1, 1, 0.3, 0.99], [1, 0, 0, 0, 0]]
         n = np.array(m)
         self.assertEqual(ds.get_positive_negative_amount(m), (2, 1))
         self.assertEqual(ds.get_positive_negative_amount(n), (2, 1))
-    
+
     def test_training_set(self):
         # should have 50% positive and 50% negative examples
         ts = ds.divide_data_into_sets(ds.get_prepared_tweet_data("realDonaldTrump", "HillaryClinton"), 0.1, 0.1, 0.8)[2]
         count = ds.get_positive_negative_amount(ts)
         self.assertEqual(count[0], count[1])
-        
+
+
 if __name__ == '__main__':
     unittest.main()
