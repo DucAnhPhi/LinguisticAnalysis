66eac30d349fbf07447029b4783d6fbfebab868e
Duc Anh Phi <ducanh.phi@hybridheroes.de>
2017-08-28 23:55:11 +0200

    feat(neural_network_tests): add unit test

--- neural_network_tests.py
+++ neural_network_tests.py
@@ -13,14 +13,20 @@ import unittest
 import neural_network as nn
 
 class NeuralNetworkTests(unittest.TestCase):
-    
+
     def test_xor(self):
         input = np.array([[0,0],[1,0],[0,1],[1,1]])
         output = np.array([[0.],[1.],[1.],[0.]])
-        net = nn.NeuralNetwork(30000,1,2,1,1,(input, output))
+        net = nn.NeuralNetwork(30000,1,2,1,1, input, output, [], [], [], [])
         net.train()
         decimal = 1
         np.testing.assert_array_almost_equal(net.forward_pass(input)[-1], output, decimal)
-        
+
+    def test_to_binary(self):
+        m = np.array([[0.9, 0.5, 0.6],[0.003, 0.4, 0.59 ],[0.8, 0.5, 0.49]])
+        binarised = np.array([[1, 0, 1],[0, 0, 1],[1, 0, 0]])
+        np.array_equal(nn.get_binary(m), binarised)
+
+
 if __name__ == '__main__':
     unittest.main()
