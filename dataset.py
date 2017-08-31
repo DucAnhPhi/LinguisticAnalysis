304050b76932d3fca61032f3a31b4c4247f228f1
Duc Anh Phi <ducanh.phi@hybridheroes.de>
2017-09-01 00:53:07 +0200

    refactor(dataset): add Dataset class and restructure code

--- dataset.py
+++ dataset.py
@@ -9,7 +9,7 @@ Prepare datasets for neural_network.py
 """
 
 import numpy as np
-import utils
+import utils as ut
 import copy
 from random import shuffle
 from nltk.corpus import cmudict
@@ -27,83 +27,6 @@ def normalize(data):
     normalized = normalized / normalized.max(axis = 0)
     return normalized
 
-def get_combined_keywords(tweets1, tweets2):
-    # get combined list of top 25 most used keywords
-    keywords1 = la.get_most_frequent_keywords(tweets1)
-    keywords2 = la.get_most_frequent_keywords(tweets2)
-
-    # get rid of tuples
-    for i,tuple in enumerate(keywords1):
-        keywords1[i] = tuple[0]
-    for i,tuple in enumerate(keywords2):
-        keywords2[i] = tuple[0]
-
-    keywords1 = set(keywords1)
-    keywords2 = set(keywords2)
-
-    # combined keywords
-    combinedKeywords = keywords1.union(keywords2)
-    # return set
-    return combinedKeywords
-
-def get_keywords_count(tweet, dictionary):
-    keywords = copy.copy(dictionary)
-    for sentence in tweet:
-        for word in sentence:
-            if word in keywords:
-                keywords[word] += 1
-    return keywords
-
-def extract_features(tweets, preprocessedTweets, keywordsCount, pronDict):
-    extracted = []
-    for index, tweet in enumerate(preprocessedTweets):
-        sentLength = la.get_sentence_length(tweet)
-        exclMarks = la.get_exclamation_marks(tweets[index])
-        gradeLvl = fk.get_flesch_grade_level(tweet, pronDict)
-        keyCount = get_keywords_count(tweet, keywordsCount)
-
-        # put all features together
-        features = [ sentLength, exclMarks, gradeLvl ]
-        for key in keyCount:
-            features.append(keyCount[key])
-        extracted.append(features)
-    # return 2D array
-    return extracted
-
-def get_prepared_tweet_data(person1, person2):
-    # get tweets
-    tweets1 = la.get_max_amount_tweets(person1)
-    tweets2 = la.get_max_amount_tweets(person2)
-
-    # remove retweets
-    tweets1 = utils.remove_retweets(tweets1)
-    tweets2 = utils.remove_retweets(tweets2)
-
-    # preprocessed tweets
-    preprocessedTweets1 = [ utils.preprocess(tweet) for tweet in tweets1 if len(utils.preprocess(tweet)) ]
-    preprocessedTweets2 = [ utils.preprocess(tweet) for tweet in tweets2 if len(utils.preprocess(tweet)) ]
-
-    # extract features
-
-    keywords = get_combined_keywords(preprocessedTweets1, preprocessedTweets2)
-    keywordsCount = {keyword:0 for keyword in keywords}
-
-    pronouncingDict = cmudict.dict()
-
-    data1 = np.array(extract_features(tweets1, preprocessedTweets1, keywordsCount, pronouncingDict))
-    data2 = np.array(extract_features(tweets2, preprocessedTweets2, keywordsCount, pronouncingDict))
-
-    # label data
-    data1 = nn.concat_bias(1, data1)
-    data2 = nn.concat_bias(0, data2)
-
-    # concatenate vertically
-    data = np.r_[data1, data2]
-    # normalize all the data
-    data = normalize(data)
-    # return numpy array
-    return data
-
 def get_positive_negative_amount(data):
     pCount = 0
     nCount = 0
@@ -115,14 +38,64 @@ def get_positive_negative_amount(data):
             nCount += 1
     return (pCount, nCount)
 
-def divide_data_into_sets(data, testAmount, cvAmount, trainingAmount):
+def get_combined_keywords(tweets1, tweets2):
+        # preprocess tweets
+        preprocTweets1 = [ ut.preprocess(tweet) for tweet in tweets1 if len(ut.preprocess(tweet)) ]
+        preprocTweets2 = [ ut.preprocess(tweet) for tweet in tweets2 if len(ut.preprocess(tweet)) ]
+
+        # get combined list of top 25 most used keywords
+        keywords1 = la.get_most_frequent_keywords(preprocTweets1)
+        keywords2 = la.get_most_frequent_keywords(preprocTweets2)
+
+        # get rid of tuples
+        for i,tuple in enumerate(keywords1):
+            keywords1[i] = tuple[0]
+        for i,tuple in enumerate(keywords2):
+            keywords2[i] = tuple[0]
+
+        keywords1 = set(keywords1)
+        keywords2 = set(keywords2)
+
+        # combined keywords
+        combinedKeywords = keywords1.union(keywords2)
+        # return dictionary for counting keywords
+        return {keyword:0 for keyword in combinedKeywords}
+
+def get_keywords_count(preprocTweet, dictionary):
+    keywords = copy.copy(dictionary)
+    for sentence in preprocTweet:
+        for word in sentence:
+            if word in keywords:
+                keywords[word] += 1
+    return keywords
+
+def extract_features(tweet, combinedKeywords, pronDict):
+    preprocessed = ut.preprocess(tweet)
+
+    # ignore empty preprocessed tweets or retweets
+    if len(preprocessed) == 0 or ut.is_retweet(tweet):
+        return []
+    else:
+        sentLength = la.get_sentence_length(preprocessed)
+        exclMarks = la.get_exclamation_marks(tweet)
+        gradeLvl = fk.get_flesch_grade_level(preprocessed, pronDict)
+        keyCount = get_keywords_count(preprocessed, combinedKeywords)
+
+        # put all features together
+        features = [ sentLength, exclMarks, gradeLvl ]
+        for key in keyCount:
+            features.append(keyCount[key])
+        # return array
+        return features
+
+def divide_data_into_sets(data, trainDataAmount):
     # count positive and negative examples
     pnCounts = get_positive_negative_amount(data)
     pCount = pnCounts[0]
     nCount = pnCounts[1]
     # initialize positive and negative count for training set
     # which contains 50% positive and 50% negative examples
-    trainingSetSize = trainingAmount * len(data)
+    trainingSetSize = trainDataAmount * len(data)
     pCount = min(pCount, nCount)
     if pCount > trainingSetSize / 2:
         pCount = int(trainingSetSize / 2)
@@ -153,5 +126,45 @@ def divide_data_into_sets(data, testAmount, cvAmount, trainingAmount):
     testSet = remainingData[0:half]
     cvSet = remainingData[half:len(remainingData)]
 
-    # return (still) labeled data sets as 2D arrays
     return (np.array(testSet), np.array(cvSet), np.array(trainingSet))
+
+
+class Dataset:
+
+    def __init__(
+        self, user1, user2, trainDataAmount
+    ):
+        self.user1 = user1
+        self.user2 = user2
+
+        self.combinedKeywords = []
+        self.pronDict = cmudict.dict()
+
+        data = self.get_prepared_tweet_data()
+        dividedData = divide_data_into_sets(data, trainDataAmount)
+        self.testSet = dividedData[0]
+        self.cvSet = dividedData[1]
+        self.trainSet = dividedData[2]
+
+    def get_prepared_tweet_data(self):
+        # get tweets and remove retweets
+        tweets1 = ut.remove_retweets(la.get_max_amount_tweets(self.user1))
+        tweets2 = ut.remove_retweets(la.get_max_amount_tweets(self.user2))
+
+        # extract features
+        self.combinedKeywords = get_combined_keywords(tweets1, tweets2)
+
+        data1 = np.array([extract_features(tweet, self.combinedKeywords, self.pronDict) for tweet in tweets1 if len(extract_features(tweet, self.combinedKeywords, self.pronDict))])
+        data2 = np.array([extract_features(tweet, self.combinedKeywords, self.pronDict) for tweet in tweets2 if len(extract_features(tweet, self.combinedKeywords, self.pronDict))])
+
+        # label data
+        data1 = nn.concat_bias(1, data1)
+        data2 = nn.concat_bias(0, data2)
+
+        # concatenate vertically
+        data = np.r_[data1, data2]
+
+        # normalize all the data
+        data = normalize(data)
+        # return numpy array
+        return data
